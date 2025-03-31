b0385d9d916cdd68c6e364dd1fa8fdc7d66d3629
Himanshu <himanshubindal90340@gmail.com>
2025-03-31 14:44:33 +0530

    url changes

--- client/src/store/useUserStore.ts
+++ client/src/store/useUserStore.ts
@@ -3,7 +3,7 @@ import { createJSONStorage, persist } from "zustand/middleware";
 import axios from "axios";
 import { LoginInputState, SignupInputState } from "@/schema/userSchema";
 import { toast } from "sonner";
-const API_END_POINT = "http://localhost:8000/api/v1/user";
+const API_END_POINT = "https://food-app-adbk.onrender.com/api/v1/user";
 
 axios.defaults.withCredentials = true;
 
